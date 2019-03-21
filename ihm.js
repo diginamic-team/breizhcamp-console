680fafdbeddf85f21337f908216b91e1d8c4e7f0
mercadier yoann <yoann.mercadier@gmail.com>
2019-03-21 17:28:33 +0100

    fin tp 3

--- ihm.js
+++ ihm.js
@@ -2,6 +2,31 @@ var service = require('./service');
 
 exports.start = function () {
     service.init(function (nb) {
-        console.log('[init]', nb, 'sessions trouvées.')
+        //console.log('[init]', nb, 'sessions trouvées.')
+        question()
     });
+    var readline = require('readline');
+
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    function question(){
+        rl.question('************************* \n 1. Rafraichir les données \n 2. Lister les sessions \n99. Quitter: ', function (saisie) {
+        if (saisie == '1') {
+            service.session(function () {
+                console.log('...Données mises à jour')
+            });
+            question()
+        }else if (saisie == '2') {
+            service.session(function (sessions) {
+                console.log(sessions)
+            });
+            question();
+        }else if (saisie == '99') {
+            rl.close();// attention, une fois l'interface fermée, la saisie n'est plus possible
+        }
+    });}
 };
+
--- service.js
+++ service.js
@@ -15,4 +15,16 @@ exports.init = function (callback) {
         });
     });
 };
+exports.session = function (callback) {
+    request('http://2018.breizhcamp.org/json/others.json', { json: true }, function (err, res, body) {
+        if (err) { return console.log('Erreur', err); }
 
+        talks = talks.concat(body);
+        request('http://2018.breizhcamp.org/json/talks.json', { json: true }, function (err, res, body) {
+            if (err) { return console.log('Erreur', err); }
+
+            talks = talks.concat(body);
+            callback(talks);
+        });
+    });
+};
