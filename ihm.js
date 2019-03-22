80dc1ad81d321bb35c35b546e4c247df1ce565af
Mélanie D <44894678+Morue@users.noreply.github.com>
2019-03-22 10:35:41 +0100

    fin tp 2

--- ihm.js
+++ ihm.js
@@ -1,20 +1,50 @@
 var service = require('./service');
 var readline = require('readline');
 
-exports.start = function() {
- 
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-    var menu = readline.createInterface({
-        input : process.stdin,
-        output: process.stdout
-    });
+
+exports.start = function () {
+    service.init(function (nb) {
+    }) 
+ 
     
-    console.log("*************************");
-    menu.question("1. Rafraichir les données\n2.xxxx\n3.yyyyy", function(saisie){
-        
-        service.init(function(nb) {
-            console.log('[init]', nb, 'sessions trouvées.')
+function poseQuestion(){
+    rl.question(
+        '*********************************** \n \n'
+        + '1. Rafraichir les données \n'
+        + '2. Lister les sessions \n'
+        + '99. Quitter \n'
+
+
+        , function (saisie) {
+
+            if (saisie == 1) {
+                service.init(function (nb) {
+                    console.log('Données mises à jour')
+                });
+                poseQuestion();
+            }
+
+            if (saisie == 2) {
+                service.listerSessions(function (nb) {
+                    console.log(nb);
+                })
+                poseQuestion();
+            }
+
+            if (saisie == 99) {
+                continuer = false;
+                rl.close();
+                console.log("*** A bientôt ***")// attention, une fois l'interface fermée, la saisie n'est plus possible
+            }
+
         });
-    })
-  
-};
+}
+    poseQuestion();
+
+ 
+}
