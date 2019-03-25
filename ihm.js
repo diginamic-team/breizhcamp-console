454cf1e0db998096ad8de5f2819f07eb939674b1
mercadier yoann <yoann.mercadier@gmail.com>
2019-03-25 09:56:25 +0100

    commence la migration

--- ihm.js
+++ ihm.js
@@ -1,9 +1,7 @@
 var service = require('./service');
 
 exports.start = function () {
-    service.init(function () {
-        question()
-    });
+    service.init(() =>  question());
     var readline = require('readline');
 
     var rl = readline.createInterface({
@@ -12,16 +10,27 @@ exports.start = function () {
     });
 
     function question(){
-        rl.question('************************* \n 1. Rafraichir les données \n 2. Lister les sessions \n99. Quitter: ', function (saisie) {
+        rl.question(`*************************
+        1. Rafraichir les données
+        2. Lister les sessions
+        3. Lister les presentateurs
+        99. Quitter: `, function (saisie) {
         if (saisie == '1') {
             service.session(function () {
-                console.log('\n...Données mises à jour')
+                console.log(`\n...Données mises à jour`)
             });
             question()
         }else if (saisie == '2') {
-            service.session(function (sessions) {
-                console.log(sessions)
-            });
+            service.session((sessions) => console.log(sessions));
+            question();
+        }else if (saisie == '3') {
+            service.presentateurs$()
+            .then((pre)=>console.log(pre))
+            .catch(()=>print(`error`));
+            question();
+        }
+        else if (saisie == '2') {
+            service.session((sessions) =>console.log(sessions));
             question();
         }else if (saisie == '99') {
             rl.close();// attention, une fois l'interface fermée, la saisie n'est plus possible
--- service.js
+++ service.js
@@ -1,27 +1,119 @@
 // tableau qui contiendra toutes les sessions du BreizhCamp
-var talks;
-var request = require('request');
+let talks;
+const request = require("request");
+let present;
+exports.init = function(callback) {
+  talks = [];
+  present = [];
+  request(
+    "http://2018.breizhcamp.org/json/others.json",
+    { json: true },
+    function(err, res, body) {
+      if (err) {
+        return console.log("Erreur", err);
+      }
 
-exports.init = function (callback) {
-    talks = [];
-    request('http://2018.breizhcamp.org/json/others.json', { json: true }, function (err, res, body) {
-        if (err) { return console.log('Erreur', err); }
+      talks = talks.concat(body);
+      request(
+        "http://2018.breizhcamp.org/json/talks.json",
+        { json: true },
+        function(err, res, body) {
+          if (err) {
+            return console.log("Erreur", err);
+          }
 
-        talks = talks.concat(body);
-        request('http://2018.breizhcamp.org/json/talks.json', { json: true }, function (err, res, body) {
-            if (err) { return console.log('Erreur', err); }
-
-            talks = talks.concat(body);
-            callback(talks.length);
-        });
-    });
-};
-exports.session = function (callback) {
-    if(talks){
-        callback(talks);
-    }else{
-        exports.init(function (nbrSession){
-            callback(talks);
-        })
+          talks = talks.concat(body);
+          callback(talks.length);
+        }
+      );
     }
+  );
 };
+
+exports.init$ = function init$() {
+  talks = [];
+  present = [];
+  return new Promise(function (resolve, reject) {
+    "http://2018.breizhcamp.org/json/others.json",
+      { json: true },
+      function (err, res, body) {
+        if (err) {
+          reject(err);
+        }
+        else {
+          const jsdom = require("jsdom");
+          const fs = require("fs");
+          const dom = new jsdom.JSDOM(body);
+          const pres = dom.window.document.querySelectorAll(".media-heading");
+          pres.forEach(function (lg) {
+            present.push(lg.innerHTML);
+            console.log(lg.innerHTML);
+          });
+          resolve(present);
+        }
+      };
+  });
+}
+
+exports.session = function(callback) {
+  if (talks) {
+    callback(talks);
+  } else {
+    exports.init(function(nbrSession) {
+      callback(talks);
+    });
+  }
+};
+
+
+
+
+exports.presentateurs$ = function presentateurs$() {
+  return new Promise(function (resolve, reject) {
+    request("http://2018.breizhcamp.org/conference/speakers", {}, function(
+      err,
+      res,
+      body
+    ) {
+      if (err) {
+        reject(err);
+      } else {
+        const jsdom = require("jsdom");
+        const fs = require("fs");
+        const dom = new jsdom.JSDOM(body);
+        const pres = dom.window.document.querySelectorAll(".media-heading");
+        pres.forEach(function(lg) {
+          present.push(lg.innerHTML);
+          console.log(lg.innerHTML);
+        });
+        resolve(present);
+      }
+    });
+  });
+}
+/*  VERSION CALLBAcK
+exports.presentateurs = function(callback) {
+  request("https://www.breizhcamp.org/conference/speakers/", {}, function(
+    err,
+    res,
+    body
+  ) {
+    if (err) {
+      callback(err);
+    }
+    const jsdom = require("jsdom");
+
+    // récupération de la page HTML exemple
+    const fs = require("fs");
+    //var pageHTML = fs.readFileSync("./prototype/unePage.html").toString();
+    // var pageHTML = fs.body.toString();
+
+    const dom = new jsdom.JSDOM(body);
+    const pres = dom.window.document.querySelectorAll(".media-heading");
+    pres.forEach(function(lg) {
+      present.push(lg.innerHTML);
+      console.log(lg.innerHTML);
+    });
+    callback(present);
+  });
+}; */
